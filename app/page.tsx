605aa35c2cd1fffbdca385ef75e36232fdaca91a
crix05 <tanmay0543@gmail.com>
2024-08-06 13:03:16 +0530

    window undefined fixed_2

--- app/page.tsx
+++ app/page.tsx
@@ -3,24 +3,26 @@ import Image from "next/image";
 import styles from "./page.module.css";
 import WebApp from '@twa-dev/sdk'
 import Script from 'next/script';
-import { useEffect } from "react";
+import { useEffect,useState } from "react";
 
 export default function Home() {
+  const [data, setData] = useState({});
   if (typeof window != 'undefined'){
     console.log(WebApp);
   }
 
-  // useEffect(() => {
-  //     if (typeof window != 'undefined'){
-  //     (window as any).onTelegramAuth = function(user: { first_name: string; last_name: string; id: string; username: string; }) {
-  //       alert('Logged in as ' + user.first_name + ' ' + user.last_name + ' (' + user.id + (user.username ? ', @' + user.username : '') + ')');
-  //     }
-  //   };
-  // }, []);
+  useEffect(() => {
+      if (typeof window != 'undefined'){
+        setData(WebApp);
+        (window as any).onTelegramAuth = function(user: { first_name: string; last_name: string; id: string; username: string; }) {
+          alert('Logged in as ' + user.first_name + ' ' + user.last_name + ' (' + user.id + (user.username ? ', @' + user.username : '') + ')');
+      }
+    };
+  }, []);
 
 
   return (
-   <div>{WebApp.initData}
+   <div>{JSON.stringify(data)}
    <Script
         src="https://telegram.org/js/telegram-widget.js?22"
         strategy="beforeInteractive" // Ensures the script loads before the page becomes interactive
